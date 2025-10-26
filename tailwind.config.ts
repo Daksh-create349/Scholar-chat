360e1fa1658f8080844c1684ecaa9cad0808799b
Daksh Shrivastav <dakshshrivastav56@gmail.com>
2025-10-26 16:31:08 +0000

    still

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -107,7 +107,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in': 'fade-in 0.5s ease-in-out',
+        'fade-in': 'fade-in 0.5s ease-in-out forwards',
         'fade-in-up': 'fade-in-up 0.5s ease-in-out forwards',
       },
     },
